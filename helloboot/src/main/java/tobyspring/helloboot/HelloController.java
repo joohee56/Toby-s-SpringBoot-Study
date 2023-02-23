a8084384a0c3c1d95c669456000eba501e81d03e
Joohui <joohuii96@gmail.com>
2023-02-23 21:30:29 +0900

    section3 : servlet standalone application

---
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,10 @@
+package tobyspring.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class HelloController {
+    public String hello(String name) {
+        return "hello " + name;
+    }
+}
