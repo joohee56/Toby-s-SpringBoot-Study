d8970589f6e060859277bf6c3da22a99130c0de8
Joohui <joohuii96@gmail.com>
2023-03-06 14:54:01 +0900

    section 4: spring standalone application

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,10 +1,22 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
+@RestController
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
     public String hello(String name) {
-        return "hello " + name;
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
